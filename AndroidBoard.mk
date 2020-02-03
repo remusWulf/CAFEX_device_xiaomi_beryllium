3ac7e2660a0f8c4d3ef7c063da027d9349cc12dd
Mulugeta Engdaw <mengdaw@codeaurora.org>
2020-02-03 00:06:39 -0800

    Redirect config mk file pick up to vendor-common
    Change-Id: I253aaa24df1c4c7d4ecf91ad37a4a3e6faa5d31c

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -117,7 +117,7 @@ endif
 #Create dsp directory
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/dsp)
 
-include device/qcom/qssi/MergeConfig.mk
+include device/qcom/vendor-common/MergeConfig.mk
 
 #----------------------------------------------------------------------
 # Radio image
